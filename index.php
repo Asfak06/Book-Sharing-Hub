acb844d5174b7dd403cfab5d67ad3b153ed25787
Asfak06 <Asfakmunna@gmail.com>
2020-01-30 21:28:25 +0600

    begin

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> log in or sign up</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+	<style>
+		 #bod{
+		 	background-color: #2c3e50;
+		 }
+         #title{
+         	font-weight: bold;        	 
+         	font-size: 40px;
+         	color:#d35400;
+         	margin-left: 400px;
+         	margin-top:70px;
+         	float:left;
+         }
+         #img1{
+         	height: 9%;
+         	width: 9%;
+            margin-left: 60px;
+         	margin-top: 50px;
+         }
+	</style>
+</head>
+
+<body id="bod">
+	<div class="head m-auto">
+		<h1 id="title" >Unversity <br> Book Sharing Hub</h1>
+		<img src="handbook.png" alt="" id="img1">
+	</div>
+</body>
+</html>
